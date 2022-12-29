1dff4bfe4b97397adad7a7c60a83dc09728d1f4d
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-12-28 23:01:09 -0600

    first commit

---
+++ server/src/server.js
@@ -0,0 +1,8 @@
+const PORT = process.env.PORT || 8000;
+const app = require("./app");
+const http = require("http");
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`listening on Port: ${PORT}...`);
+});

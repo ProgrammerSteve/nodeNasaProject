013662d7f68e842b1051e21a270ff3799abfc117
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-12-30 18:13:55 -0600

    set up launches router, controller and model

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -6,7 +6,7 @@ const launch = {
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
-  destination: "Kepler-442 b",
+  target: "Kepler-442 b",
   customer: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
@@ -19,9 +19,14 @@ function getAllLaunches() {
 }
 function addNewLaunch(lauch) {
   latestFlightNumber++;
-  launch.set(
+  launches.set(
     latestFlightNumber,
-    Object.assign(launch, { flightNumber: latestFlightNumber })
+    Object.assign(launch, {
+      upcoming: true,
+      success: true,
+      customer: ["ZTM", "NASA"],
+      flightNumber: latestFlightNumber,
+    })
   );
 }
 

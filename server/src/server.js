45bdf4ba0ba92cb08831c9ec5f3e1884956956a9
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-01-01 16:40:20 -0600

    connected to mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,22 @@
+require("dotenv").config();
 const PORT = process.env.PORT || 8000;
+const MONGO_URL = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.arc3nbp.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
+const mongoose = require("mongoose");
 const app = require("./app");
 const http = require("http");
 const server = http.createServer(app);
+
 const { loadPlanetsData } = require("./models/planets.model");
 
+mongoose.connection.once("open", () => {
+  console.log("mongoDB connection ready!");
+});
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`listening on Port: ${PORT}...`);
